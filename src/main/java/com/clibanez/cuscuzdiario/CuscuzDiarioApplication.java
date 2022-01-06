16411129f2b21fa64bb1b9a6b5f2fa2f4695f029
Clibanez <clibanezzcaldas@gmail.com>
2022-01-06 07:57:39 -0300

    Iniciando o projeto.

---
+++ src/main/java/com/clibanez/cuscuzdiario/CuscuzDiarioApplication.java
@@ -0,0 +1,13 @@
+package com.clibanez.cuscuzdiario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuscuzDiarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CuscuzDiarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clibanez/cuscuzdiario/CuscuzDiarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clibanez.cuscuzdiario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CuscuzDiarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
